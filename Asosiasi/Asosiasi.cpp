9357741c00ac63684fd0d0e9c85478558b68671b
nauvalachid <nauvalachid@gmail.com>
2023-05-22 19:04:50 +0700

    menambahkan objek "dokter" mencetak daftar dokter yang menangani pasien menambahkan objek "pasien"

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -32,3 +32,18 @@ public:
 	void tambahPasien(pasien*);
 	void cetakPasien();
 };
+
+void pasien::tambahDokter(dokter* pDokter) {
+	daftar_dokter.push_back(pDokter);
+}
+void pasien::cetakDokter() {
+	cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+	for (auto& a : daftar_dokter) {
+		cout << a->nama << "\n";
+	}
+	cout << endl;
+}
+	void dokter::tambahPasien(pasien* pPasien) {
+		daftar_pasien.push_back(pPasien);
+		pPasien->tambahDokter(this);
+	}
