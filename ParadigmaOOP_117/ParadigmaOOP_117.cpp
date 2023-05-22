66b53f5462f917a21f7548ed24ec727070be6d54
nauvalachid <nauvalachid@gmail.com>
2023-05-22 11:31:33 +0700

    membuat class orang

--- ParadigmaOOP_117/ParadigmaOOP_117.cpp
+++ ParadigmaOOP_117/ParadigmaOOP_117.cpp
@@ -2,14 +2,20 @@
 #include <string>
 using namespace std;
 
-class orang{
+class orang {
 public:
 	string nama;
 
 	orang(string pNama) :
-		nama(pNama){
+		nama(pNama) {
 		cout << "orang dibuat\n" << endl;
 	}
 	~orang() {
 		cout << "orang dihapus\n" << endl;
 	}
+
+	int jumlah(int a, int b) {
+		return a + b;
+	}
+
+};
