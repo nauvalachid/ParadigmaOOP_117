24fd33109a7df1c2c16a197d18c30f0d83f86680
nauvalachid <nauvalachid@gmail.com>
2023-05-22 19:08:42 +0700

    mencetak data pasien

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -47,3 +47,10 @@ void pasien::cetakDokter() {
 		daftar_pasien.push_back(pPasien);
 		pPasien->tambahDokter(this);
 	}
+	void dokter::cetakPasien() {
+		cout << "Daftar Pasien dari dokter \"" << this->nama << "\":\n";
+		for (auto& a : daftar_pasien) {
+			cout << a->nama << "\n";
+		}
+		cout << endl;
+	}
